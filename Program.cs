4f57f6c439631b3db28af45026c061d7ccb38745
jeromecagado <jerome.cagado@outlook.com>
2025-08-11 14:32:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿namespace Assignment_6._1._1
+{
+    internal class Program
+    {
+        public enum HouseType
+        {
+            None = 0,
+            RANCH = 1,
+            COLONIAL = 2,
+            VICTORIAN = 3
+        }
+        class Node
+        {
+            public int Number { get; }
+            public string Address { get; }
+            public HouseType Data {  get; set; }
+            public Node? Next { get; set; }
+
+            public Node(int number, string address, HouseType house)
+            {
+                Number = number;
+                Address = address;
+                Data = house;
+                Next = null;
+            }
+
+        }
+
+        class LinkedNode
+        {
+            private Node? head;
+            private Node? tail;
+            private int size;
+            public int Size { get { return size; } }
+
+            public LinkedNode()
+            {
+                head = tail = null;
+                size = 0;
+            }
+
+            public bool IsEmpty()
+            {
+                return size == 0;
+            }
+
+            public void AddFirst(int number, string address, HouseType type)
+            {
+                Node? newNode = new Node(number, address, type);
+                if (IsEmpty())
+                {
+                    head = tail = newNode;
+                }
+                else
+                {
+                    newNode.Next = head;
+                    head = newNode;
+                }
+                size++;
+            }
+
+            public void Display()
+            {
+                if (IsEmpty())
+                {
+                    throw new ArgumentException("Linked List is empty");
+                }
+
+                Node curr = head;
+                while (curr != null)
+                {
+                    Console.Write(curr.Data);
+                    if (curr.Next != null) Console.Write(" --> ");
+                    curr = curr.Next;
+                }
+            }
+
+            public void AddLast(int number, string address, HouseType type)
+            {
+                Node newNode = new Node(number, address, type);
+                if (IsEmpty())
+                {
+                    head = tail = newNode;
+                }
+                else
+                {
+                    tail.Next = newNode;
+                    tail = newNode;
+                }
+                size++;
+            }
+
+            public Node? SearchHouse(int number)
+            {
+                if(IsEmpty())
+                {
+                    throw new ArgumentException("Linked List is empty");
+                }
+                if(number <= 0 || number > size)
+                {
+                    throw new ArgumentException("number is not valid.");
+                }
+                if (number == 1) return head;
+                if (number == size) return tail;
+
+                Node curr = head;
+                int i = 1;
+                while (curr != null)
+                {
+                    if (i == number)
+                    {
+                        return curr;
+                    }
+                    curr = curr.Next;
+                    i++;
+                }
+                Console.WriteLine("No house found");
+                return null;
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            LinkedNode list = new();
+            list.AddFirst(101, "123 Num st.", HouseType.COLONIAL);
+            list.AddLast(204, "345 Beach drive.", HouseType.RANCH);
+            list.AddFirst(305, "789 Mariners Way.", HouseType.VICTORIAN);
+            list.Display();
+            Console.WriteLine($"\nSize of the array is: {list.Size}");
+
+            Console.Write("\nEnter house number to search: ");
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                Node node = list.SearchHouse(num);
+                Console.WriteLine(node != null ? $"Found: {node.Data}" : "House not found.");
+            }
+        }
+    }
+}
+
